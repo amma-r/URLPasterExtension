f9f8d2c3c31df85d5381e44434e613964319912c
Ammar <118911270+amma-r@users.noreply.github.com>
2025-08-15 12:08:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const ulEl = document.getElementById("ul-el")
 const saveBtn = document.getElementById("save-el")
 const inputEl = document.getElementById("input-el")
+let leadsFromLocalStorage = localStorage.getItem("urls")
 let urls = []
 let listItems = ""
 
-console.log(saveBtn)
+if(leadsFromLocalStorage != null)
+{
+    urls = JSON.parse(localStorage.getItem("urls"))
+
+}
+
+renderUrls()
+
+
 saveBtn.addEventListener("click", function() {
     getInputValue() 
     listItems = ""       
-    for (let i=0; i<urls.length; i++)
-    {
-        listItems += `<li><a href='${urls[i]}' target='__blank'>${urls[i]}</a></li>`
-    }
-    ulEl.innerHTML = listItems
+    renderUrls()
 })
 
 function getInputValue()
 {
     urls.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("urls", JSON.stringify(urls))
 }
 
+function renderUrls()
+{
+    listItems = ""   
+    for (let i=0; i<urls.length; i++)
+    {
+        listItems += `
+        <li>
+            <a href='${urls[i]}' target='__blank'>
+                ${urls[i]}            
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
